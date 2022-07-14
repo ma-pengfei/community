94cf12f85fc30ed2a80ad54c42326f5c518775f0
mapengfei <2337869522@qq.com>
2022-07-14 09:44:11 +0800

    Spring Boot进阶，开发社区核心功能 事务管理 拦截器 统一处理异常@ControllerAdvice 统一记录日志AOP

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -1,4 +1,4 @@
-package com.nowcoder.community.controller.advice;
+package com.nowcoder.community.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,7 +7,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
